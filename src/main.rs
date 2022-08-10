c3ed53b1fa88ff2569cc85c005cfd73376e9fb12
Manoel <manoel.lobo@gmail.com>
2022-08-09 21:25:26 -0300

    real world version

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-use std::{
-    fs::File,
-    io::{self, Read},
-};
+use std::{fs, io};
 
 fn main() {
     let name = read_username_from_file().unwrap();
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut s = String::new();
-    File::open("try.txt")?.read_to_string(&mut s)?;
-    Ok(s)
+    fs::read_to_string("try.txt")
 }
