3f111ea9caa640c406c595dc3aae58ca4b161fa8
Manoel <manoel.lobo@gmail.com>
2022-08-09 21:11:19 -0300

    propagating errors

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use std::{fs::File, io::ErrorKind};
+use std::{
+    fs::File,
+    io::{self, Read},
+};
 
 fn main() {
-    let f = File::open("try.txt").expect("Failed to open file");
+    let name = read_username_from_file().unwrap();
+}
 
-    // let f = match f {
-    //     Ok(file) => file,
-    //     Err(error) => match error.kind() {
-    //         ErrorKind::NotFound => match File::create("try.txt") {
-    //             Ok(file_created) => file_created,
-    //             Err(error) => panic!("Unable to create file: {error:?}"),
-    //         },
-    //         other_error => {
-    //             panic!("Unable to open file: {other_error:?}")
-    //         }
-    //     },
-    // };
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("try.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
